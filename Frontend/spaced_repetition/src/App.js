79d7231a71db0c7d62c03ff12c49e550f3f7d9e2
Lennart Giesken <lennartgiesken@outlook.com>
2021-11-03 19:15:04 +0100

    Component for topics added

---
+++ Frontend/spaced_repetition/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import FrameComponent from './components/frameComponent';
+
+function App() {
+  return (
+    <FrameComponent />
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
---
+++ Frontend/spaced_repetition/src/components/nameComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import '../index.css'
+
+
+class NameComponent extends React.Component {
+    render() { 
+        return <div className="nameComponent">Fach x</div>;
+    }
+}
+ 
+export default NameComponent;
