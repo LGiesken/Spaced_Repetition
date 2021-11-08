42cb0ce4e77c06b45dcdcc0031dc213ce1f1df5b
Lennart Giesken <lennartgiesken@outlook.com>
2021-11-08 18:56:48 +0100

    +-Symbol added

--- Frontend/spaced_repetition/src/App.js
+++ Frontend/spaced_repetition/src/App.js
@@ -1,26 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import FrameComponent from './components/frameComponent';
+import MenuComponent from './components/menuComponent';
 
 function App() {
-  return (
+  return ( <div>
     <FrameComponent />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <MenuComponent />
+    </div>
+    
   );
 }
 
